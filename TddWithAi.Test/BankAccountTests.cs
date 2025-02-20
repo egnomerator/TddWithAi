fcbc3b8a6f52869b308ce12bd530b303e18021f6
egnomerator <egnomerator@users.noreply.github.com>
2025-02-20 15:55:56 -0600

    new solution with one dummy test

---
+++ TddWithAi.Test/BankAccountTests.cs
@@ -0,0 +1,25 @@
+﻿namespace TddWithAi.Test;
+
+public class BankAccountTests
+{
+    [Fact]
+    public void DummyTest()
+    {
+        Assert.True(true);
+    }
+    
+    [Fact]
+    public void ShouldGetAccountName()
+    {
+        // arrange
+        const string accountName = "TestAccount";
+
+        // act
+        var sut = new BankAccount(accountName);
+        var actualAccountName = sut.GetName();
+
+        // assert
+        Assert.Equal(accountName, actualAccountName);
+
+    }
+}
